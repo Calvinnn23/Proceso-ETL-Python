5231cb780e42bc0ba3aa8421bd66654ad8dae8e7
Calvin <creativomiel@gmail.com>
2025-03-19 00:35:37 -0500

    Update test_etl.py

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Agregar el directorio 'src' al sys.path
+# Agrega directorio "src" al sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 src_path = os.path.join(current_dir, "..", "src")
 if src_path not in sys.path:
@@ -14,8 +14,8 @@ import pandas as pd
 
 
 class TestETL(unittest.TestCase):
+    # Prueba la limpieza de datos con Pandas
     def setUp(self):
-        # Prepara un pequeño DataFrame de ejemplo para pruebas
         self.sample_data = {
             "TestSheet": pd.DataFrame(
                 {"id": [1, 2, 2, 3], "value": [10, None, None, 30]}
@@ -26,7 +26,6 @@ class TestETL(unittest.TestCase):
         transformer = Transformer(self.sample_data)
         cleaned = transformer.clean_data_pandas()
         df_clean = cleaned["TestSheet"]
-        # Verifica que se hayan eliminado duplicados y que los nulos se hayan rellenado
         self.assertEqual(len(df_clean), 3)
         self.assertFalse(df_clean.isnull().values.any())
 
